8da33cd17b8b261830b3b15d734d0604669860ce
LaisBonin <boninlaisc@gmail.com>
2022-05-23 21:00:01 -0300

    Add settings

---
+++ lib/design/my_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color primaryyellow = Color(0xFFEFC984);
+  static const Color primarygreen = Color(0xFF45674F);
+  static const Color primarydark = Color(0xFF10351B);
+  static const Color primarywhite = Color(0xFFF4E8D2);
+}
